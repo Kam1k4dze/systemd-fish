bdb6a2dcd3ca4d3ac2ac2b18aaa41b461e20b43b
Kamikadze <40305144+Kam1k4dze@users.noreply.github.com>
2025-05-05 15:32:35 +0500

    Update systemd.fish

--- conf.d/systemd.fish
+++ conf.d/systemd.fish
@@ -15,33 +15,33 @@ set power_commands hibernate hybrid-sleep poweroff reboot suspend
 
 # --- Create abbreviations for user commands ---
 for cmd in $user_commands
-    abbr --add sc-$cmd "systemctl $cmd "
-    abbr --add scu-$cmd "systemctl --user $cmd "
+    abbr --add sc-$cmd "systemctl $cmd"
+    abbr --add scu-$cmd "systemctl --user $cmd"
 end
 
 # --- Create abbreviations for sudo commands ---
 for cmd in $sudo_commands
-    abbr --add sc-$cmd "sudo systemctl $cmd "
-    abbr --add scu-$cmd "systemctl --user $cmd "
+    abbr --add sc-$cmd "sudo systemctl $cmd"
+    abbr --add scu-$cmd "systemctl --user $cmd"
 end
 
 # --- Create abbreviations for power commands ---
 for cmd in $power_commands
-    abbr --add sc-$cmd "systemctl $cmd "
+    abbr --add sc-$cmd "systemctl $cmd"
 end
 
 # --- --now commands (explicitly defined) ---
-abbr --add sc-enable-now "sudo systemctl enable --now "
-abbr --add sc-disable-now "sudo systemctl disable --now "
-abbr --add sc-mask-now "sudo systemctl mask --now "
+abbr --add sc-enable-now "sudo systemctl enable --now"
+abbr --add sc-disable-now "sudo systemctl disable --now"
+abbr --add sc-mask-now "sudo systemctl mask --now"
 
-abbr --add scu-enable-now "systemctl --user enable --now "
-abbr --add scu-disable-now "systemctl --user disable --now "
-abbr --add scu-mask-now "systemctl --user mask --now "
+abbr --add scu-enable-now "systemctl --user enable --now"
+abbr --add scu-disable-now "systemctl --user disable --now"
+abbr --add scu-mask-now "systemctl --user mask --now"
 
 # --- --failed commands ---
-abbr --add sc-failed "systemctl --failed "
-abbr --add scu-failed "systemctl --user --failed "
+abbr --add sc-failed "systemctl --failed"
+abbr --add scu-failed "systemctl --user --failed"
 
 # unset variables
 set -e user_commands sudo_commands power_commands
